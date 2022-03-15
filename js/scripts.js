f152320c2fb05df805d1bcee49bfef191e751fed
Shiburizu <shiburizu312@gmail.com>
2022-03-15 17:23:30 -0400

    why does the forward slash keep getting removed?

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ window.onload = function()
     else
     {
         jQuery(document).ready(function(){
-            jQuery.get('js/quotes.txt', function(txt) {
+            jQuery.get('/js/quotes.txt', function(txt) {
                 var quotes = txt.split("\n");
                 var randLineNum = Math.floor(Math.random()*(quotes.length));
                 jQuery('#quote').append(quotes[randLineNum]);
